6c26ec3a33f50b3c5301908b29c5ab3440bb49ac
Andrew <Andrew.na92@gmail.com>
2023-07-09 11:38:06 -0400

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -10,11 +10,3 @@ from matplotlib import cm
 from torchmetrics import MeanAbsoluteError, MeanAbsolutePercentageError
 from torch import nn, optim, Tensor
 from torch.utils.data import Dataset, DataLoader, random_split
-
-# script parameters
-batch_size = 128
-log_interval = 100
-
-# run on GPU if possible
-cuda = torch.cuda.is_available()
-device = torch.device("cuda" if cuda else "cpu")
